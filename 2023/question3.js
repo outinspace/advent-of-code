be6aa0b1af7299e0aa080a93e95733d11e133773
Nicholas Wilson <nick.wilson@aviture.us.com>
2023-12-03 16:21:46 -0600

    question 3

---
+++ 2023/question3.js
@@ -0,0 +1,49 @@
+import Bun from "bun";
+
+const input = Bun.file("input3.txt");
+const contents = await input.text();
+const lines = contents.split("\n");
+
+const allNumbers = []
+
+lines.forEach((line, lineIndex) => {
+    const numbers = [...line.matchAll(/\d+/g)]
+    numbers.forEach(match => {
+        console.log({ match }, match.index)
+
+        allNumbers.push({
+            string: match[0],
+            number: parseInt(match[0]),
+            y1: lineIndex,
+            x1: match.index
+        })
+    })
+});
+
+const containsSymbol = (x1, x2, y) => {
+    const line = lines[y] ?? ''
+    const string = line.substring(x1, x2)
+
+    return string.match(/[^.\d]/) !== null
+}
+
+const isAdjacentToSymbol = ({ string, x1, y1 }) => {
+    const x2 = x1 + string.length - 1 // TODO
+
+    const isAdjacent = containsSymbol(x1 - 1, x1, y1)
+        || containsSymbol(x2 + 1, x2 + 2, y1)
+        || containsSymbol(x1 - 1, x2 + 2, y1 - 1)
+        || containsSymbol(x1 - 1, x2 + 2, y1 + 1)
+
+    console.log({ string, isAdjacent, x1, x2, y1 })
+
+    return isAdjacent
+}
+
+const goodNumbers = allNumbers.filter(isAdjacentToSymbol)
+console.log({ allNumbers })
+
+let sum = 0
+goodNumbers.forEach(_ => sum += _.number)
+
+console.log({ sum })
